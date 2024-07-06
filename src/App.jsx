007c878bd76a9b87512eb1e33b78c744472c49aa
cheoljundev <cheoljundev@gmail.com>
2024-07-07 05:34:24 +0900

    조건적 콘텐츠 렌더링 : 변수 + 조건문 사용

--- src/App.jsx
+++ src/App.jsx
@@ -7,48 +7,53 @@ import {useState} from "react";
 function App() {
     const [selectedTopic, setSelectedTopic] = useState()
 
-    function handleSelect(selectedButton){
+    function handleSelect(selectedButton) {
         // selectedButton -> 'components', 'jsx', 'props', 'state'
         setSelectedTopic(selectedButton);
     }
 
     console.log("APP COMPONENT EXECUTING")
 
-  return (
-    <div>
-        <Header/>
-        <main>
-            <section id="core-concepts">
-                <h2>Core Concepts</h2>
-                <ul>
-                    <CoreConcept {...CORE_CONCEPTS[0]}/>
-                    <CoreConcept {...CORE_CONCEPTS[1]}/>
-                    <CoreConcept {...CORE_CONCEPTS[2]}/>
-                    <CoreConcept {...CORE_CONCEPTS[3]}/>
-                </ul>
-            </section>
-            <section id="examples">
-                <h2>Examples</h2>
-                <menu>
-                    <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
-                    <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-                    <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-                    <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
-                </menu>
-                {!selectedTopic && <p>Please select a topic.</p>}
-                {selectedTopic && (
-                    <div id="tab-content">
-                        <h3>{EXAMPLES[selectedTopic].title}</h3>
-                        <p>{EXAMPLES[selectedTopic].description}</p>
-                        <pre>
-                            <code>{EXAMPLES[selectedTopic].code}</code>
-                        </pre>
-                    </div>
-                )}
-            </section>
-        </main>
-    </div>
-  );
+    let tabContent = <p>Please select a topic.</p>;
+
+    if (selectedTopic) {
+        tabContent = (
+            <div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                    <code>{EXAMPLES[selectedTopic].code}</code>
+                </pre>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <Header/>
+            <main>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcept {...CORE_CONCEPTS[0]}/>
+                        <CoreConcept {...CORE_CONCEPTS[1]}/>
+                        <CoreConcept {...CORE_CONCEPTS[2]}/>
+                        <CoreConcept {...CORE_CONCEPTS[3]}/>
+                    </ul>
+                </section>
+                <section id="examples">
+                    <h2>Examples</h2>
+                    <menu>
+                        <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+                        <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+                        <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+                        <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+                    </menu>
+                </section>
+                {tabContent}
+            </main>
+        </div>
+    );
 }
 
 export default App;
