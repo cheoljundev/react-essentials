fda2f178cb1de05b8f6169dd0dd64ffa509ae8f9
cheoljundev <cheoljundev@gmail.com>
2024-07-07 18:48:58 +0900

    컴포넌트 타입 동적으로 설정하기 : container 설정

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -28,7 +28,9 @@ export default function Examples(){
 
     return (
         <Section id="examples" title="Examples">
-            <Tabs buttons={
+            <Tabs
+                ButtonsContainer="menu"
+                buttons={
                 <>
                     <TabButton isSelected={selectedTopic === 'components'}
                                onClick={() => handleSelect('components')}>Components</TabButton>
