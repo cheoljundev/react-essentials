4deaa64a65d16682abc7dc412eea4ab5a54393bf
cheoljundev <cheoljundev@gmail.com>
2024-07-07 18:52:07 +0900

    기본 Prop(속성) 값 설정

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -29,7 +29,6 @@ export default function Examples(){
     return (
         <Section id="examples" title="Examples">
             <Tabs
-                ButtonsContainer="menu"
                 buttons={
                 <>
                     <TabButton isSelected={selectedTopic === 'components'}
