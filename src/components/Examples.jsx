71f1fce7a432551b28c01aef126370eb4e984157
cheoljundev <cheoljundev@gmail.com>
2024-07-07 18:29:36 +0900

    내부 요소에 Props(속성)이 전달되지 않을 경우

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,6 +1,7 @@
 import TabButton from "./TabButton";
 import {EXAMPLES} from "../data";
 import {useState} from "react";
+import Section from "./Section";
 
 export default function Examples(){
     const [selectedTopic, setSelectedTopic] = useState()
@@ -25,8 +26,7 @@ export default function Examples(){
     }
 
     return (
-        <section id="examples">
-            <h2>Examples</h2>
+        <Section id="examples" title="Examples">
             <menu>
                 <TabButton isSelected={selectedTopic === 'components'}
                            onSelect={() => handleSelect('components')}>Components</TabButton>
@@ -37,6 +37,6 @@ export default function Examples(){
                            onSelect={() => handleSelect('state')}>State</TabButton>
             </menu>
             {tabContent}
-        </section>
+        </Section>
     );
 }
