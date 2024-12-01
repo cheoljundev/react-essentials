174db558b8afcac3f2d6454c2c150c55d81d54b2
cheoljundev <cheoljundev@gmail.com>
2024-12-01 22:14:06 +0900

    컴포넌트 구성: 특별한 “children” Prop (“자녀” 속성)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import {CORE_CONCEPTS} from "./data";
 import Header from "./components/Header/Header";
+import TabButton from "./components/TabButton";
 
 function CoreConcept({image, title, description}) {
   return (<li>
@@ -23,6 +24,15 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]}/>
             <CoreConcept {...CORE_CONCEPTS[3]}/>
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
