c01d2bf8518285bd94a56c94342a5929d8cbc06e
cheoljundev <cheoljundev@gmail.com>
2024-12-04 21:25:13 +0900

    이벤트 처리하기 [핵심 개념]

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,5 +1,14 @@
 export default function TabButton({children}) {
+
+  function handleClick() {
+    console.log('Hello, World!');
+  }
+
   return (
-    <li><button>{children}</button></li>
+    <li>
+      <button onClick={handleClick}> {/* 함수 이름만 전달(포인터만 전달)하는 것이 선언형 프로그래밍의 방법 중 하나다. */}
+        {children}
+      </button>
+    </li>
   );
 }
