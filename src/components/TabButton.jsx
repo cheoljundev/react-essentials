87c8fe3a1252464505ab6c183791e8bc0be3bc97
cheoljundev <cheoljundev@gmail.com>
2024-12-11 16:36:32 +0900

    함수를 Prop(속성)의 값으로 전달하기

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,12 +1,8 @@
-export default function TabButton({children}) {
-
-  function handleClick() {
-    console.log('Hello, World!');
-  }
+export default function TabButton({children, onSelect}) {
 
   return (
     <li>
-      <button onClick={handleClick}> {/* 함수 이름만 전달(포인터만 전달)하는 것이 선언형 프로그래밍의 방법 중 하나다. */}
+      <button onClick={onSelect}> {/* 함수 이름만 전달(포인터만 전달)하는 것이 선언형 프로그래밍의 방법 중 하나다. */}
         {children}
       </button>
     </li>
